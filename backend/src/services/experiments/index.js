dd4ff0aa2e5351f2fe82165919376958ba6076dd
Admin <Admin@admins-mac-pro.tainstruments.com>
2017-05-12 21:00:53 -0400

    start to flush out experiments service experiments service will listen to heartbeats and runstate when runstate transitions to 'Test' it will create an experiment in the database and start saving real time signals to the database

--- backend/src/services/experiments/index.js
+++ backend/src/services/experiments/index.js
@@ -5,6 +5,8 @@ const hooks = require('./hooks');
 class Service {
   constructor(options) {
     this.options = options || {};
+    this.heartbeats = []
+    this.runStateDictionary = {}
   }
 
   /*
@@ -17,6 +19,13 @@ class Service {
       console.log(path);
 
       this.app = app;
+
+      //COUCHBASE
+      var couchbase = require('couchbase');
+      var cluster = new couchbase.Cluster('couchbase://127.0.0.1');
+      var bucket = cluster.openBucket('experiments');
+
+      this.bucket = bucket;
   }
 
   find(params) {
@@ -64,6 +73,57 @@ module.exports = function(){
 
   // Set up our after hooks
   experimentsService.after(hooks.after);
+
+  /////NATS TEST
+  var NATS = require('nats');
+  var nats = NATS.connect();
+
+  // Simple Subscriber
+  //MSG IS INSTRUMENT ID
+  nats.subscribe('heartbeat', function(heartbeat)
+  {
+    //if( !(experimentsService.heartbeats.includes(heartbeat)))
+    if(!(heartbeat in experimentsService.runStateDictionary))
+    {
+      experimentsService.runStateDictionary[heartbeat] = {runstate:''}
+
+      //listen for run state changes
+      var sid = nats.subscribe(heartbeat + '.runstate', function(runstate) {
+        if(experimentsService.runStateDictionary[heartbeat].runstate != runstate)
+        {
+          console.log('experiments:' + runstate)
+          experimentsService.runStateDictionary[heartbeat].runstate = runstate
+
+          if(runstate == 'Test')
+          {
+            console.log('experiments, creating experiment')
+          }
+        }
+        //itemService.update(msg, instrumentInfoDict[msg]);
+        //itemService.updateDB(msg, instrumentInfoDict[msg]);
+
+        //itemService.requestNotification(instrumentInfoDict[msg], runstate);
+      });
+
+      //listen for real time signals
+      var sid = nats.subscribe(heartbeat + '.realtimesignalsstatus', function(response) {
+        //console.log(runstate);
+
+        //if we are in the middle of a test write real time signals to database
+        if(experimentsService.runStateDictionary[heartbeat].runstate == "Test")
+        {
+          console.log('writing real time signals to database')
+          //instrumentInfoDict[msg].realtimesignalsstatus = response;
+
+          //TEMPORARILY STOP NOTIFYING REAL TIME SIGNALS
+          //itemService.update(msg, instrumentInfoDict[msg]);
+        }
+      });
+
+    }
+
+  });
+
 };
 
 module.exports.Service = Service;
